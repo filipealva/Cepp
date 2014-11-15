698073e921bd23f45cfdc16660b9af11c5e41dde
filipealva <ofilipealvarenga@gmail.com>
2014-11-15 21:48:08 -0200

    Header added to details UITableView

--- Cepp/Controller/CPPCepDetailsTableViewController.swift
+++ Cepp/Controller/CPPCepDetailsTableViewController.swift
@@ -31,18 +31,6 @@ class CPPCepDetailsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
-    }
-
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
